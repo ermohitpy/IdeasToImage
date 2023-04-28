518a18c6286c95baa358fd7512afe4164c6dc699
Mohit <orange@OMMAC20.local>
2023-04-28 17:40:33 +0530

    add Back Handler

--- src/Navigator/index.tsx
+++ src/Navigator/index.tsx
@@ -4,10 +4,59 @@ import { SCREENS } from '../constant';
 import Login from '../screens/Login';
 import Main from '../screens/main';
 import Splash from '../screens/Splash';
+import React from 'react';
+import {BackHandler,Alert} from 'react-native';
 const{LOGIN,MAIN,SPLASH} = SCREENS;
 
 export default function Navigator() {
     const Stack = createNativeStackNavigator();
+    const navigationRef:any =  React.createRef()
+
+    React.useEffect(() => {
+      function handleBackButton() {
+        const routeInfo = navigationRef.current.getCurrentRoute();
+        if (
+          routeInfo.name.toLowerCase() === LOGIN ||
+          routeInfo.name.toLowerCase() === MAIN
+        ) {
+          exitApp();
+        } else {
+          if (navigationRef.current.canGoBack()) {
+            navigationRef.current.goBack();
+          }
+        }
+        return true;
+      }
+  
+      // Handle when back button pressed
+      const backHandler = BackHandler.addEventListener(
+        "hardwareBackPress",
+        handleBackButton
+      );
+      return () => backHandler.remove();
+    }, []);
+  
+    // ****************************** exit function ***********************************
+    const exitApp = () => {
+      Alert.alert(
+        "Exit App",
+        "Exiting the application?",
+        [
+          {
+            text: "Cancel",
+            onPress: () => { },
+            style: "cancel",
+          },
+          {
+            text: "OK",
+            onPress: () => BackHandler.exitApp(),
+          },
+        ],
+        {
+          cancelable: false,
+        }
+      );
+    };
   return (
     <NavigationContainer>
         <Stack.Navigator screenOptions={{headerShown:false}} initialRouteName={SPLASH}>
